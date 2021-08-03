ab6bc89dca6ff143d18c910c51c9bd5d959111cf
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-08-03 15:39:36 +0200

    favicon buging

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Nyolcagú Jóga Alapítvány`,
-        short_name: `Nyolcág`,
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `./static/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-gatsby-cloud`,
@@ -38,8 +38,6 @@ module.exports = {
         display: "swap",
       },
     },
-
-    `gatsby-plugin-offline`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
