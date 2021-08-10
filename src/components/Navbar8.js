8b78c6f0a3b8e6e6ea86f253c5f72241103d5ed8
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-08-10 09:41:59 +0200

    navbar changis

--- src/components/Navbar8.js
+++ src/components/Navbar8.js
@@ -5,7 +5,7 @@ import Navbar from "react-bootstrap/Navbar"
 import Nav from "react-bootstrap/Nav"
 import AnimeLogo from "./AnimeLogo"
 import "../sass/components/_navbar8.scss"
-import { Container, Row } from "react-bootstrap"
+// import { Container, Row } from "react-bootstrap"
 
 export default function Navbar8() {
   return (
@@ -16,41 +16,39 @@ export default function Navbar8() {
       variant="light"
       sticky="top"
     >
-      <Container fluid>
-        <Navbar.Brand href="/">
-          <AnimeLogo alt="nyolcág logo" />
-        </Navbar.Brand>
-        <Navbar.Brand id="BrandNameFont">NYOLCÁGÚ JÓGA ALAPÍTVÁNY</Navbar.Brand>
-        <Navbar.Brand id="nyolcag">NYOLCÁG</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav as="ul">
-            <Nav.Item as="li">
-              <Link to="/">FŐOLDAL</Link>
-            </Nav.Item>
+      <Navbar.Brand href="/">
+        <AnimeLogo alt="nyolcág logo" />
+      </Navbar.Brand>
+      <Navbar.Brand id="BrandNameFont">NYOLCÁGÚ JÓGA ALAPÍTVÁNY</Navbar.Brand>
+      <Navbar.Brand id="nyolcag">NYOLCÁG</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav as="ul">
+          <Nav.Item as="li">
+            <Link to="/">FŐOLDAL</Link>
+          </Nav.Item>
 
-            <Nav.Item as="li">
-              <Link to="/rolunk">RÓLUNK</Link>
-            </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/rolunk">RÓLUNK</Link>
+          </Nav.Item>
 
-            <Nav.Item as="li">
-              <Link to="/projektjeink">PROJEKTJEINK</Link>
-            </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/projektjeink">PROJEKTJEINK</Link>
+          </Nav.Item>
 
-            <Nav.Item as="li">
-              <Link to="/blog">BLOG</Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Link to="/kapcsolat">KAPCSOLAT</Link>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Link className="btn btn-primary __button-color" to="/tamogatas">
-                TÁMOGATÁS
-              </Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+          <Nav.Item as="li">
+            <Link to="/blog">BLOG</Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link to="/kapcsolat">KAPCSOLAT</Link>
+          </Nav.Item>
+          <Nav.Item as="li">
+            <Link className="btn btn-primary __button-color" to="/tamogatas">
+              TÁMOGATÁS
+            </Link>
+          </Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   )
 }
--- src/pages/adatvedelem.js
+++ src/pages/adatvedelem.js
@@ -6,6 +6,7 @@ import Container from "react-bootstrap/Container"
 import Row from "react-bootstrap/Row"
 import Col from "react-bootstrap/Col"
 import "bootstrap/dist/css/bootstrap.min.css"
+import "../sass/components/_adatvedelem.scss"
 
 export default function Adatvedelem() {
   return (
