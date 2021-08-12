087c28db959f8ffb1bf7c1e0d9bcb39164977fa5
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-08-12 14:43:53 +0200

    debuging 88

--- src/pages/kapcsolat.js
+++ src/pages/kapcsolat.js
@@ -1,5 +1,5 @@
 import React from "react"
-import Layout from "../components/Layout"
+import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Button from "react-bootstrap/Button"
 import { Row, Col } from "react-bootstrap"
