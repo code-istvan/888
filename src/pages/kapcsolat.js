b047d076c00a528a5774ec2d86b534f6a5a66486
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-08-12 12:55:19 +0200

    contact form

--- src/pages/kapcsolat.js
+++ src/pages/kapcsolat.js
@@ -1,12 +1,45 @@
 import React from "react"
-import LayoutKapcsolat from "../components/LayoutKapcsolat"
+import Layout from "../components/Layout"
 import Seo from "../components/seo"
+import Button from "react-bootstrap/Button"
+import { Row, Col } from "react-bootstrap"
 
 export default function Kapcsolat() {
   return (
-    <LayoutKapcsolat>
+    <Layout>
       <Seo title="Kapcsolat" />
       <h1>Kapcsolat</h1>
-    </LayoutKapcsolat>
+      <form
+        name="contact 8ag"
+        method="post"
+        data-netlify="true"
+        onSubmit="submit"
+      >
+        <input type="hidden" name="form-name" value="contact 8ag" />
+
+        <Row>
+          <Col>
+            <input type="text" name="name" placeholder="Név*" required />
+          </Col>
+          <Col>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              placeholder="*"
+              placeholder="E-mail cím*"
+            />
+          </Col>
+        </Row>
+
+        <Row>
+          <Col>
+            <textarea name="comments" placeholder="Üzenet*" required></textarea>
+          </Col>
+        </Row>
+
+        <Button type="submit">Küldés</Button>
+      </form>
+    </Layout>
   )
 }
