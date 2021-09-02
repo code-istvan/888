df766fd3d3bb28fe8a39dd7d327b917a0cc8d54b
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-09-02 20:45:34 +0200

    cms problem

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,35 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sass`,
-    `gatsby-plugin-netlify-cms`,
+
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        manualInit: true,
+        modulePath: `${__dirname}/src/cms/cms.ts`,
+        publicPath: "cms",
+        htmlFavicon: "src/assets/logoCircle.png",
+        customizeWebpackConfig: (config, { stage, plugins }) => {
+          config.resolve = {
+            ...config.resolve,
+            alias: {
+              ...config.resolve.alias,
+              path: require.resolve("path-browserify"),
+            },
+            fallback: {
+              ...config.resolve.fallback,
+              fs: false,
+              child_process: false,
+              module: false,
+            },
+          }
+          if (stage === "build-javascript" || stage === "develop") {
+            config.plugins.push(plugins.provide({ process: "process/browser" }))
+          }
+          config.plugins.push(plugins.provide({ Buffer: ["buffer", "Buffer"] }))
+        },
+      },
+    },
 
     {
       resolve: `gatsby-source-filesystem`,
