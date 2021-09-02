53f79c2830ebc944592431707d2ad1f75977c554
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-09-02 15:13:40 +0200

    1

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
     `gatsby-plugin-sass`,
-    "gatsby-plugin-netlify-cms",
+    // "gatsby-plugin-netlify-cms",
 
     {
       resolve: `gatsby-source-filesystem`,
