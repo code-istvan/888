8591d56e96eddbd316e0faf13bd135e661b6548f
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-09-06 13:51:47 +0200

    refress

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,15 +16,16 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `blog`,
+        path: `${__dirname}/blog`,
       },
     },
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `blog`,
-        path: `${__dirname}/blog/`,
+        name: `images`,
+        path: `${__dirname}/src/images`,
       },
     },
 
