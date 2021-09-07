e8f7c16c604f3dcda84fb8d5bbfff94bf56b37d5
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-09-07 16:40:25 +0200

    yes i could done

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,25 +1,46 @@
 import { graphql } from "gatsby"
 import React from "react"
+import { Row, Col } from "react-bootstrap"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-export default function Blog({ data }) {
-  console.log(data.site)
-
+const Blog = ({ data }) => {
+  const posts = data.allMdx.nodes
   return (
     <Layout>
       <Seo title="Blog" />
-      <h1>BLOG</h1>
+      <Row>
+        <Col>
+          <h1>BLOG</h1>
+          {posts.map(post => {
+            const title = post.frontmatter.title
+            return (
+              <Row>
+                <h1>{title}</h1>
+                <p>{post.frontmatter.date}</p>
+                <p>{post.frontmatter.description}</p>
+              </Row>
+            )
+          })}
+        </Col>
+      </Row>
     </Layout>
   )
 }
 
+export default Blog
+
 export const pageQuery = graphql`
   {
-    site {
-      siteMetadata {
-        title
-        description
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      nodes {
+        excerpt
+        slug
+        frontmatter {
+          date(formatString: "YYYY MMMM DD")
+          description
+          title
+        }
       }
     }
   }
