8733476deac08c1d7deea870bbfb144c60a369e9
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-09-13 13:48:27 +0200

    blog fixed

--- src/templates/blogPosts.js
+++ src/templates/blogPosts.js
@@ -1,4 +1,4 @@
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 import React from "react"
 import { Row, Col } from "react-bootstrap"
@@ -6,17 +6,19 @@ import Container from "react-bootstrap/Container"
 import Button from "react-bootstrap/Button"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { GatsbyImage } from "gatsby-plugin-image"
+import "../sass/components/_blogposts.scss"
 
-const blogPosts = ({ data, pageContext }) => {
+const blogPosts = ({ data }) => {
   const { frontmatter, body } = data.mdx
-  // const { previous, next } = pageContext
 
   return (
     <Layout>
       <Container>
         <Seo title="Blog" />
         <Row>
-          <Col>
+          <Col></Col>
+          <Col xs={10}>
             <h1>{frontmatter.title}</h1>
             <p>{frontmatter.date}</p>
             <img src={frontmatter.thumbnail} alt={frontmatter.title} />
@@ -28,6 +30,7 @@ const blogPosts = ({ data, pageContext }) => {
             </Button>
             <br /> <br />
           </Col>
+          <Col></Col>
         </Row>
       </Container>
     </Layout>
