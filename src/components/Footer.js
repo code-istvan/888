438a946b38c0c2b3f6c37bbf214b2c5429a32070
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-09-20 11:56:21 +0200

    footer change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -20,10 +20,9 @@ export default function Footer() {
       <Row>
         <Col className="footer__first_block">
           <p>
-            “A test nehézkes, az elme vibráló, a lélek sugárzó. A jóga
-            gyakorlása kibontakoztatja a testben rejlő lehetőséget és a vibráló
-            elme szintjére emelik, hogy mindkettő - a vibrálóvá vált test és
-            elme - a lélek fénye felé fordulhasson.”
+            “ Az odaadással végzett astanga gyakorlás által elérhető a fizikai,
+            mentális és lelki boldogság amely a szívünkben lévő szabadsághoz
+            vezet.”
           </p>
           <br />
         </Col>
