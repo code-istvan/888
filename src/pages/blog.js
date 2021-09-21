c6553958505cd0aea62c8cb2247a1f478a0b2928
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-09-21 23:37:53 +0200

    new try

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,14 @@
-import { graphql, Link } from "gatsby"
 import React from "react"
 import { Row, Col } from "react-bootstrap"
 import Container from "react-bootstrap/Container"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import Image from "react-bootstrap/Image"
 import BlogRoll from "../components/BlogRoll"
 import "../sass/components/card-hover.scss"
 import "../sass/components/_blog.scss"
 import "../sass/components/card-hover.scss"
-import { GatsbyImage } from "gatsby-plugin-image"
 
-const Blog = ({ data }) => {
+const Blog = () => {
   return (
     <Layout>
       <Seo title="Blog" />
