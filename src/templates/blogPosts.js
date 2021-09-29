5b1de60b4ef56ecc46883e31b013781c6ee1977c
code-istvan <63305129+code-istvan@users.noreply.github.com>
2021-09-29 16:51:44 +0200

    blogpost card

--- src/templates/blogPosts.js
+++ src/templates/blogPosts.js
@@ -26,24 +26,24 @@ const blogPosts = ({ data }) => {
           description={frontmatter.description}
           thumbnail={src}
         />
-        <Row>
-          <Col md={{ span: 8, offset: 2 }}>
-            <h1>{frontmatter.title}</h1>
-            <Row xs="auto">
+        <Row className="blogPosts__card">
+          <Col md={{ span: 8, offset: 2 }} className="blogPost__backgrondColor">
+            <h2>{frontmatter.title}</h2>
+            <ul>
               {" "}
-              <Col className="blogPosts__same-line">
+              <li className="blogPosts__same-line">
                 <AuthorIcon />
                 <p>{frontmatter.author}</p>
-              </Col>
-              <Col className="blogPosts__same-line">
+              </li>
+              <li className="blogPosts__same-line">
                 <DateIcon />
                 <p>{frontmatter.date}</p>
-              </Col>
-              <Col className="blogPosts__same-line">
+              </li>
+              <li className="blogPosts__same-line">
                 <TagsIcon />
                 <p>{frontmatter.tags}</p>
-              </Col>
-            </Row>
+              </li>
+            </ul>
             <GatsbyImage image={image} alt={frontmatter.title} />
             <br />
             <br />
@@ -88,75 +88,3 @@ export const query = graphql`
     }
   }
 `
-
-// import { graphql } from "gatsby"
-// import { MDXRenderer } from "gatsby-plugin-mdx"
-// import React from "react"
-// import { Row, Col } from "react-bootstrap"
-// import Container from "react-bootstrap/Container"
-// import Button from "react-bootstrap/Button"
-// import Layout from "../components/layout"
-// import AuthorIcon from "../components/AuthorIcon"
-// import DateIcon from "../components/DateIcon"
-// import TagsIcon from "../components/TagsIcon"
-// import Seo from "../components/seo"
-// import { navigate } from "gatsby"
-// import "../sass/components/_blogposts.scss"
-
-// const blogPosts = ({ data }) => {
-//   const { frontmatter, body } = data.mdx
-//   const image = frontmatter.thumbnail || ""
-//   return (
-//     <Layout>
-//       <Container>
-//         <Seo
-//           title={frontmatter.title}
-//           description={frontmatter.description}
-//           thumbnail={image}
-//         />
-//         <Row>
-//           <Col></Col>
-//           <Col xs={10}>
-//             <h1>{frontmatter.title}</h1>
-//             <div className="blogPosts__same-line">
-//               <AuthorIcon />
-//               <p>{frontmatter.author}</p>
-//               <DateIcon />
-//               <p>{frontmatter.date}</p>
-//               <TagsIcon />
-//               <p>{frontmatter.tags}</p>
-//             </div>
-//             <img src={frontmatter.thumbnail} alt={frontmatter.title} />
-//             <article>
-//               <MDXRenderer>{body}</MDXRenderer>
-//             </article>
-//             {/* <Button variant="primary" href="/blog"> */}
-//             <Button variant="primary" onClick={() => navigate(-1)}>
-//               Vissza
-//             </Button>
-//             <br /> <br />
-//           </Col>
-//           <Col></Col>
-//         </Row>
-//       </Container>
-//     </Layout>
-//   )
-// }
-
-// export default blogPosts
-
-// export const query = graphql`
-//   query PostsBySlug($slug: String!) {
-//     mdx(fields: { slug: { eq: $slug } }) {
-//       body
-//       frontmatter {
-//         title
-//         description
-//         author
-//         tags
-//         date(formatString: "YYYY. MM. DD.")
-//         thumbnail
-//       }
-//     }
-//   }
-// `
